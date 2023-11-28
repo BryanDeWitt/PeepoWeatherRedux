ac7acdf3bb2ca9d8b9198cc4e1a2fc57cc1b34a3
Bryan <tolovehayley@gmail.com>
2023-11-28 03:07:57 -0300

    ahora si arreglados los iconos

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,9 +1,14 @@
 import { useContext } from 'react'
 import { CityContext } from '../context/CityContext'
+import feelsLikeIcon from './feelsLike.png'
+import windIcon from './wind.png'
+import humidityIcon from './humidity.png'
+import visibilityIcon from './visibility.png'
 import './CurrentWeather.css'
 
 export function CurrentWeather ({ weather, error, loading }) {
   const { cityName } = useContext(CityContext)
+
   if (error) {
     return <h2>{error}</h2>
   }
@@ -25,26 +30,24 @@ export function CurrentWeather ({ weather, error, loading }) {
           {weather.location.localtime.split(' ')[1]}, {weather.current.temp_c}°C
         </h2>
         <div className='current-weather-condition'>
-          <h3>
-            {weather.current.condition.text}
-          </h3>
+          <h3>{weather.current.condition.text}</h3>
           <img src={weather.current.condition.icon} width={100} height={100} alt='' />
         </div>
         <div className='current-weather-details'>
           <div className='details-cards'>
-            <img src='./feelsLike.png' alt='Feels like Icon' />
+            <img src={feelsLikeIcon} alt='Feels like Icon' />
             <p>Feels like: {weather.current.feelslike_c}°C</p>
           </div>
           <div className='details-cards'>
-            <img src='./wind.png' alt='Wind velocity icon' />
+            <img src={windIcon} alt='Wind velocity icon' />
             <p>Wind: {weather.current.wind_kph}km/h</p>
           </div>
           <div className='details-cards'>
-            <img src='./humidity.png' alt='Humidity icon' />
+            <img src={humidityIcon} alt='Humidity icon' />
             <p>Humidity: {weather.current.humidity}%</p>
           </div>
           <div className='details-cards'>
-            <img src='./visibility.png' alt='Visibility icon' />
+            <img src={visibilityIcon} alt='Visibility icon' />
             <p>Visibility: {weather.current.vis_km}km</p>
           </div>
         </div>
